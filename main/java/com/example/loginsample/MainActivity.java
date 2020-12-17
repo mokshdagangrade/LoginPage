67b9a1792b3264897576849aedd56d89a31ca3c1
mokshdagangrade <68953506+mokshdagangrade@users.noreply.github.com>
2020-12-17 17:52:54 +0530

    Add files via upload

---
+++ main/java/com/example/loginsample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.loginsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mbutton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mbutton= findViewById(R.id.Btnlogout);
+        mbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
